c8d2595e846b84aba277e53b7c2b1bb288693c26
Manassarn Manoonchai <manassarn@gmail.com>
2018-06-02 18:31:07 +0700

    :construction: Add SMSSender.send_message/2

--- apps/sms_sender/lib/sms_sender.ex
+++ apps/sms_sender/lib/sms_sender.ex
@@ -15,4 +15,17 @@ defmodule SMSSender do
   def hello do
     :world
   end
+
+  @doc """
+  Send message to specified phone number
+
+  ## Examples
+
+      iex> SMSSender.send_message("Hello", "0812345678")
+      {:ok, "Message Sent"}
+
+  """
+  def send_message(message, phone_number) do
+    {:ok, "Message Sent"}
+  end
 end
--- apps/sms_sender/test/sms_sender_test.exs
+++ apps/sms_sender/test/sms_sender_test.exs
@@ -5,4 +5,10 @@ defmodule SMSSenderTest do
   test "greets the world" do
     assert SMSSender.hello() == :world
   end
+
+  describe "send_message/2" do
+    test "sends message to phone number" do
+      assert {:ok, _} = SMSSender.send_message("Hello from ShypChoice", "0863949474")
+    end
+  end
 end
