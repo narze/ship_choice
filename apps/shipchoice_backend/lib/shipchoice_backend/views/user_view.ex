dd08b382c40695dc3be9070514ba543ba210ddba
Manassarn Manoonchai <manassarn@gmail.com>
2018-10-01 23:49:52 +0700

    :lipstick: Show user remaining credits

--- apps/shipchoice_backend/lib/shipchoice_backend/views/user_view.ex
+++ apps/shipchoice_backend/lib/shipchoice_backend/views/user_view.ex
@@ -1,7 +1,7 @@
 defmodule ShipchoiceBackend.UserView do
   use ShipchoiceBackend, :view
   import Scrivener.HTML
-  alias ShipchoiceDb.User
+  alias ShipchoiceDb.{Credits, User}
 
   def is_admin(%User{} = user) do
     if user.is_admin do
@@ -9,5 +9,9 @@ defmodule ShipchoiceBackend.UserView do
     else
       "No"
     end
+  end
+
+  def get_credits(%User{} = user) do
+    Credits.get_user_credit(user)
   end
 end
