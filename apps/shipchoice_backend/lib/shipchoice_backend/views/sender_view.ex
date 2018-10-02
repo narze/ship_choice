8718bea6da3a3d4d574f8819d609008ccb093cf2
Manassarn Manoonchai <manassarn@gmail.com>
2018-10-03 01:21:47 +0700

    :recycle: Refactor views & controllers

--- apps/shipchoice_backend/lib/shipchoice_backend/views/sender_view.ex
+++ apps/shipchoice_backend/lib/shipchoice_backend/views/sender_view.ex
@@ -1,4 +1,9 @@
 defmodule ShipchoiceBackend.SenderView do
   use ShipchoiceBackend, :view
   import Scrivener.HTML
+  alias ShipchoiceDb.{Credits, Sender}
+
+  def get_credits(%Sender{} = sender) do
+    Credits.get_sender_credit(sender)
+  end
 end
