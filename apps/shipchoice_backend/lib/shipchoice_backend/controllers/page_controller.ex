20ec8e3acf744f582581776902a948692c226370
Manassarn Manoonchai <manassarn@gmail.com>
2018-10-12 01:27:35 +0700

    :construction: Add link to sender dashboard

--- apps/shipchoice_backend/lib/shipchoice_backend/controllers/page_controller.ex
+++ apps/shipchoice_backend/lib/shipchoice_backend/controllers/page_controller.ex
@@ -2,6 +2,26 @@ defmodule ShipchoiceBackend.PageController do
   use ShipchoiceBackend, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    if current_user = conn.assigns[:current_user] do
+
+      current_user =
+        current_user
+        |> ShipchoiceDb.Repo.preload(:senders)
+
+      senders = current_user.senders
+
+      render(
+        conn,
+        "index.html",
+        senders: senders,
+        current_user: current_user
+      )
+    else
+      render(
+        conn,
+        "index.html",
+        current_user: nil
+      )
+    end
   end
 end
--- apps/shipchoice_backend/test/shipchoice_backend/controllers/page_controller_test.exs
+++ apps/shipchoice_backend/test/shipchoice_backend/controllers/page_controller_test.exs
@@ -1,8 +1,28 @@
 defmodule ShipchoiceBackend.PageControllerTest do
   use ShipchoiceBackend.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to ShypChoice!"
+  alias Ecto.Adapters.SQL.Sandbox
+  alias ShipchoiceDb.Repo
+
+  import ShipchoiceDb.Factory
+
+  setup do
+    :ok = Sandbox.checkout(Repo)
+  end
+
+  describe "GET /" do
+    test "renders index with welcome message", %{conn: conn} do
+      conn = get(conn, "/")
+      assert html_response(conn, 200) =~ "Welcome to ShypChoice!"
+    end
+
+    test "renders link to sender when logged in and owned a sender", %{conn: conn} do
+      user = insert(:user)
+      sender = insert(:sender, users: [user])
+      conn = assign(conn, :current_user, user)
+
+      conn = get(conn, "/")
+      assert html_response(conn, 200) =~ sender.name
+    end
   end
 end
