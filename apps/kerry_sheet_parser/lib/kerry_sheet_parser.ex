686874b35be79f78bfa611ba07dd249832e2add7
Manassarn Manoonchai <manassarn@gmail.com>
2018-11-03 18:17:07 +0700

    :tada: Init KerrySheetParser umbrella app

---
+++ apps/kerry_sheet_parser/lib/kerry_sheet_parser.ex
@@ -0,0 +1,18 @@
+defmodule KerrySheetParser do
+  @moduledoc """
+  Documentation for KerrySheetParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KerrySheetParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/kerry_sheet_parser/test/kerry_sheet_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule KerrySheetParserTest do
+  use ExUnit.Case
+  doctest KerrySheetParser
+
+  test "greets the world" do
+    assert KerrySheetParser.hello() == :world
+  end
+end
