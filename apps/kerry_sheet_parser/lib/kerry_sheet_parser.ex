48f516775cd9a7b8d53498e4c1b260e5a93c57ab
Manassarn Manoonchai <manassarn@gmail.com>
2018-11-14 01:30:41 +0700

    :sparkles: Parse kerry pending sheet

--- apps/kerry_sheet_parser/lib/kerry_sheet_parser.ex
+++ apps/kerry_sheet_parser/lib/kerry_sheet_parser.ex
@@ -2,17 +2,56 @@ defmodule KerrySheetParser do
   @moduledoc """
   Documentation for KerrySheetParser.
   """
+  @keys [
+    :consignment_no,
+    nil,
+    :date_time,
+    nil,
+    nil,
+    nil,
+    nil,
+    :payer,
+    :sender,
+    :route,
+    :dc,
+    :svc_type,
+    :pkg_count,
+    :last_status_code,
+    :dly_status_code,
+    nil,
+    nil,
+    nil,
+    :dly_status_remark,
+    nil,
+    :station_location,
+    :cod_amount,
+  ]
 
-  @doc """
-  Hello world.
+  def parse_pending_sheet(sheet_path) do
+    sheet_number = 0
+    start_row = 0
 
-  ## Examples
+    data = Excelion.parse!(sheet_path, sheet_number, start_row)
 
-      iex> KerrySheetParser.hello()
-      :world
+    rows =
+      data
+      |> Enum.drop(14) # Drop rows until first row of data
+      |> Enum.map(fn row ->
+        row =
+          row
+          |> Enum.map(fn cell ->
+            cell
+            |> String.replace_prefix("\"", "")
+            |> String.replace_suffix("\"", "")
+            |> String.replace("\n", "")
+          end)
 
-  """
-  def hello do
-    :world
+        @keys
+        |> Enum.zip(row)
+        |> Enum.into(%{})
+        |> Map.delete(nil)
+      end)
+
+    {:ok, rows}
   end
 end
