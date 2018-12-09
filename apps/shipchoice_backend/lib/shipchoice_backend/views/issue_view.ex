2dc6c24731c050f14c4801a4de7afb5a629f4b2b
Manassarn Manoonchai <manassarn@gmail.com>
2018-12-09 16:40:24 +0700

    :lipstick: Show resolved time as absolute datetime

--- apps/shipchoice_backend/lib/shipchoice_backend/views/issue_view.ex
+++ apps/shipchoice_backend/lib/shipchoice_backend/views/issue_view.ex
@@ -5,7 +5,14 @@ defmodule ShipchoiceBackend.IssueView do
 
   def resolved_at(%Issue{} = issue) do
     if issue.resolved_at do
-      issue.resolved_at |> Timex.format!("{relative}", :relative)
+      abs =
+        issue.resolved_at
+        |> Timex.format!("%T %F", :strftime)
+      rel =
+        issue.resolved_at
+        |> Timex.format!("({relative})", :relative)
+
+      abs <> " " <> rel
     else
       "-"
     end
